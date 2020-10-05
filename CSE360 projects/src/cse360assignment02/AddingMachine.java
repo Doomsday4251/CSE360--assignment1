5d19ab22693f3c2654e4ed902d7986dee525141a
Secret <Secret@Secret-Desktop>
2020-10-05 14:50:44 -0700

    original

--- CSE360 projects/src/cse360assignment02/AddingMachine.java
+++ CSE360 projects/src/cse360assignment02/AddingMachine.java
@@ -2,33 +2,54 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  private int myCalculator = 0;
-  private String thesequence = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
-    
   }
   
   public int getTotal () {
-    return myCalculator;
+    return 0;
   }
   
   public void add (int value) {
-	  myCalculator = myCalculator + value;
-	  thesequence = thesequence + " + value";
   }
 
   public void subtract (int value) {
-	  myCalculator = myCalculator - value;
-	  thesequence = thesequence + " - value";
   }
 
   public String toString () {
-	  int theequation = total;
     return "";
   }
 
   public void clear() {
   }
 }
+/*
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  private String thesequence = "0";
+  public AddingMachine() {
+    total = 0;
+  }
+  public int getTotal () {
+    return total;
+  }
+  public void add (int value) {
+	  total = total + value;
+	  thesequence = thesequence + " + value";
+  }
+  public void subtract (int value) {
+	  total = total - value;
+	  thesequence = thesequence + " - value";
+  }
+
+  public String toString () {
+    return thesequence;
+  }
+  public void clear() {
+	  System.gc();
+  } 
+}
+ */
---
+++ CSE360 projects/src/cse360assignment02/main.java
@@ -0,0 +1,8 @@
+package cse360assignment02;
+
+public class main {
+	public static void main(String[] args) {
+		//AddingMachine myCalculator;
+		//myCalculator.add (4);
+	}
+}
